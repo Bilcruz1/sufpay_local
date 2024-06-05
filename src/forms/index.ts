903a958cf359890d7f67d94aeebf552de70fa0fb
James Adah <adahjames11@gmail.com>
2024-06-05 12:34:40 +0100

    done

--- src/forms/index.ts
+++ src/forms/index.ts
@@ -4,3 +4,4 @@ export { default as LoginForm } from "./LoginForm";
 export { default as ForgetPasswordForm } from "./ForgetPasswordForm";
 export { default as SignupForm } from "./SignupForm";
 export { default as CreateNewPasswordForm } from "./CreateNewPasswordForm";
+export { default as VerifyAccountForm } from "./VerifyAccountForm";
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -5,3 +5,4 @@ export { default as SignInPage } from "./SignInPage"
 export { default as ForgetPasswordPage } from "./ForgetPasswordPage";
 export { default as SignUpPage } from "./SignUpPage";
 export { default as CreateNewPasswordPage } from "./CreateNewPasswordPage";
+export { default as VerifyAccountPage } from "./VerifyAccountPage";
