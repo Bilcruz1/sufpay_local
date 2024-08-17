d18f9521c00e4ddf03bc09463f2b6810b595da0f
James Adah <adahjames11@gmail.com>
2024-08-17 16:37:41 +0100

    added scroll to dash

--- src/components/dashboard/DashboardHistories.tsx
+++ src/components/dashboard/DashboardHistories.tsx
@@ -13,7 +13,6 @@ import {
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-
 function createData(
   name: string,
   calories: number,
@@ -30,6 +29,7 @@ const rows = [
   createData("Eclair", 262, 16.0, 24, 6.0),
   createData("Cupcake", 305, 3.7, 67, 4.3),
   createData("Gingerbread", 356, 16.0, 49, 3.9),
+  // Add more rows as needed
 ];
 
 const DashboardHistories: React.FC<{ title: string; linkUrl: string }> = ({
@@ -46,70 +46,138 @@ const DashboardHistories: React.FC<{ title: string; linkUrl: string }> = ({
         borderRadius: "5px",
         width: "100%",
         height: "35vh",
+        display: "flex",
+        flexDirection: "column",
       }}
     >
-      {/* top */}
+      {/* Top Section */}
       <Box
         sx={{
           display: "flex",
           justifyContent: "space-between",
           alignItems: "center",
+          marginBottom: "0.5rem",
         }}
       >
         <Typography variant="h5">{title}</Typography>
-        <Link onClick={() => navigate(linkUrl)}>View all</Link>
+        <Link
+          component="button"
+          variant="body2"
+          onClick={() => navigate(linkUrl)}
+          sx={{ cursor: "pointer" }}
+        >
+          View all
+        </Link>
       </Box>
 
-      {/* body */}
-      <Box
+      {/* Table Section */}
+      <TableContainer
+        component={Paper}
         sx={{
-        //   maxHeight: "400px", // Adjust the height as needed
-          overflowY: "auto", // Enable vertical scrolling
+          flexGrow: 1,
+          boxShadow: "none",
+          maxHeight: "100%",
+          overflow: "auto",
           "&::-webkit-scrollbar": {
-            width: "6px", // Width of the scrollbar
+            width: "6px",
           },
           "&::-webkit-scrollbar-thumb": {
-            backgroundColor: "#888", // Color of the scrollbar thumb
-            borderRadius: "10px", // Rounded corners
+            backgroundColor: "#888",
+            borderRadius: "10px",
           },
           "&::-webkit-scrollbar-thumb:hover": {
-            backgroundColor: "#555", // Darker color on hover
+            backgroundColor: "#555",
           },
           "&::-webkit-scrollbar-track": {
-            backgroundColor: "#f1f1f1", // Background of the scrollbar track
+            backgroundColor: "#f1f1f1",
           },
         }}
       >
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Dessert (100g serving)</TableCell>
-                <TableCell align="right">Calories</TableCell>
-                <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                <TableCell align="right">Protein&nbsp;(g)</TableCell>
+        <Table stickyHeader sx={{ minWidth: 650, border: 0 }}>
+          <TableHead>
+            <TableRow>
+              <TableCell
+                sx={{
+                  position: "sticky",
+                  top: 0,
+                  backgroundColor: "#fff",
+                  borderBottom: 0,
+                  fontWeight: "bold",
+                }}
+              >
+                Transactions
+              </TableCell>
+              <TableCell
+                align="right"
+                sx={{
+                  position: "sticky",
+                  top: 0,
+                  backgroundColor: "#fff",
+                  borderBottom: 0,
+                  fontWeight: "bold",
+                }}
+              >
+                Amount
+              </TableCell>
+              <TableCell
+                align="right"
+                sx={{
+                  position: "sticky",
+                  top: 0,
+                  backgroundColor: "#fff",
+                  borderBottom: 0,
+                  fontWeight: "bold",
+                }}
+              >
+                Status
+              </TableCell>
+              <TableCell
+                align="right"
+                sx={{
+                  position: "sticky",
+                  top: 0,
+                  backgroundColor: "#fff",
+                  borderBottom: 0,
+                  fontWeight: "bold",
+                }}
+              >
+                Date
+              </TableCell>
+              <TableCell
+                align="right"
+                sx={{
+                  position: "sticky",
+                  top: 0,
+                  backgroundColor: "#fff",
+                  borderBottom: 0,
+                  fontWeight: "bold",
+                }}
+              >
+               
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.name}>
+                <TableCell sx={{ borderBottom: 0 }}>{row.name}</TableCell>
+                <TableCell align="right" sx={{ borderBottom: 0 }}>
+                  {row.calories}
+                </TableCell>
+                <TableCell align="right" sx={{ borderBottom: 0 }}>
+                  {row.fat}
+                </TableCell>
+                <TableCell align="right" sx={{ borderBottom: 0 }}>
+                  {row.carbs}
+                </TableCell>
+                <TableCell align="right" sx={{ borderBottom: 0 }}>
+                  {row.protein}
+                </TableCell>
               </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow
-                  key={row.name}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.name}
-                  </TableCell>
-                  <TableCell align="right">{row.calories}</TableCell>
-                  <TableCell align="right">{row.fat}</TableCell>
-                  <TableCell align="right">{row.carbs}</TableCell>
-                  <TableCell align="right">{row.protein}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Box>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 };
