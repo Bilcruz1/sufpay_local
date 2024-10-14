bf3227af593e6cc3ff9572a7f02565d1f6e7b149
Muhammed Bilal Omeiza <104626610+Bilcruz1@users.noreply.github.com>
2024-10-14 01:12:30 +0100

    settings page

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,41 +1,43 @@
 //sign up
-export { default as SignUpLayout } from "./signup/SignupLayout";
-export { default as Rhs } from "./signup/Rhs";
-export { default as InputFeild } from "./signup/InputFeild";
-export { default as PasswordInputFeild } from "./signup/PasswordInputFeild";
-export { default as PhoneNumberField } from "./signup/PhoneNumberField";
-export { default as PasswordChks } from "./signup/PasswordChks"//"./signup/PasswordChks";
-export { default as TextAreaInputFeild } from "./signup/TextAreaInputFeild";
+export { default as SignUpLayout } from './signup/SignupLayout';
+export { default as Rhs } from './signup/Rhs';
+export { default as InputFeild } from './signup/InputFeild';
+export { default as PasswordInputFeild } from './signup/PasswordInputFeild';
+export { default as PhoneNumberField } from './signup/PhoneNumberField';
+export { default as PasswordChks } from './signup/PasswordChks'; //"./signup/PasswordChks";
+export { default as TextAreaInputFeild } from './signup/TextAreaInputFeild';
 
 //landing
-export { default as Banner } from "./landingpage/Banner";
-export { default as FeatureSectionOne } from "./landingpage/WhatWeDo";
-export { default as Footer } from "./landingpage/Footer";
-export { default as Header } from "./landingpage/Header";
-export { default as Nav } from "./landingpage/Nav";
-export { default as ContactUs } from "./landingpage/ContactUs";
-export { default as WhatWeDo } from "./landingpage/WhatWeDo";
-export { default as AboutUs } from "./landingpage/AboutUs";
-export { default as CoreValues } from "./landingpage/CoreValues";
-export { default as OurServices } from "./landingpage//OurServices";
-export { default as Carousel } from "./landingpage/Carousel";
+export { default as Banner } from './landingpage/Banner';
+export { default as FeatureSectionOne } from './landingpage/WhatWeDo';
+export { default as Footer } from './landingpage/Footer';
+export { default as Header } from './landingpage/Header';
+export { default as Nav } from './landingpage/Nav';
+export { default as ContactUs } from './landingpage/ContactUs';
+export { default as WhatWeDo } from './landingpage/WhatWeDo';
+export { default as AboutUs } from './landingpage/AboutUs';
+export { default as CoreValues } from './landingpage/CoreValues';
+export { default as OurServices } from './landingpage//OurServices';
+export { default as Carousel } from './landingpage/Carousel';
 
 // template
-export { default as DashboardTemplate } from "./template/DashboardTemplate";
-export { default as LandingTemplate } from "./template/LandingTemplate";
+export { default as DashboardTemplate } from './template/DashboardTemplate';
+export { default as LandingTemplate } from './template/LandingTemplate';
 
 // dashboard
-export { default as DashHeader } from "./dashboard/DashHeader";
-export { default as DashSideBar } from "./dashboard/DashSideBar";
-export {default as DashboardHistories} from "./dashboard/landing/DashboardHistories";
-export { default as DashboardLanding } from "./dashboard/landing/DashboardLanding";
-export { default as DonutChart } from "./dashboard/landing/DonutChat";
-export { default as DashboardCards } from "./dashboard/landing/DashboardCards";
+export { default as DashHeader } from './dashboard/DashHeader';
+export { default as DashSideBar } from './dashboard/DashSideBar';
+export { default as DashboardHistories } from './dashboard/landing/DashboardHistories';
+export { default as DashboardLanding } from './dashboard/landing/DashboardLanding';
+export { default as DonutChart } from './dashboard/landing/DonutChat';
+export { default as DashboardCards } from './dashboard/landing/DashboardCards';
 
-// transactions 
-export { default as AirtimeTransactions } from "./dashboard/transactions/AirtimeTransactions";
+// transactions
+export { default as AirtimeTransactions } from './dashboard/transactions/AirtimeTransactions';
+
+//settings
+export { default as SettingsPage } from './settings/landingpage/settingsPage';
 
 //utils
-export { default as Loading } from "./Loading";
-export {default as Notification} from "./Notification";
-
+export { default as Loading } from './Loading';
+export { default as Notification } from './Notification';
